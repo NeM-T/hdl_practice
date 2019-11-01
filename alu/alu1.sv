c82f8991da0cb4a4358f2fea2da26cbe2de83e0d
NeM-T <persodou@keio.jp>
2019-11-01 17:50:37 +0900

    fix

--- alu/alu1.sv
+++ alu/alu1.sv
@@ -11,11 +11,11 @@ module alu #(parameter width = 16)
     );
  
  logic signed [width -1: 0] w1, w2, w3;
- mux_in mux_1(x, select[5: 4], w1);
- mux_in mux_2(y, select[3: 2], w2);
+ mux_in #(width) mux_1(x, select[5: 4], w1);
+ mux_in #(width) mux_2(y, select[3: 2], w2);
  
- mux2 mux_3(w1 & w2, w1 + w2, select[1], w3);
- mux2 mux_4(w3, ~w3, select[0], out);
+ mux2 #(width) mux_3(w1 & w2, w1 + w2, select[1], w3);
+ mux2 #(width) mux_4(w3, ~w3, select[0], out);
  
  logic an[width-1: 0];
  assign an[0] = out[0] | out[1];
@@ -33,7 +33,7 @@ module mux_in #(parameter width = 16)
     input logic [1: 0] selec,
     output logic signed [width-1: 0] a);
 
-    mux4 mux_in1(in, in ^ in, ~in, 0, selec, a);
+    mux4 #(width) mux_in1(in, in ^ in, ~in, 0, selec, a);
 endmodule
 
 module mux4 #(parameter width = 16)
@@ -42,9 +42,9 @@ module mux4 #(parameter width = 16)
     output logic signed [width-1: 0] out2);
 
 logic signed [width-1: 0] wire1, wire2;
-mux2 sel1(n1, n2, sele[0], wire1);
-mux2 sel2(n3, n4, sele[0], wire2);
-mux2 selout(wire1, wire2, sele[1], out2);
+mux2 #(width) sel1(n1, n2, sele[0], wire1);
+mux2 #(width) sel2(n3, n4, sele[0], wire2);
+mux2 #(width) selout(wire1, wire2, sele[1], out2);
 endmodule
 
 module mux2 #(parameter width = 16)
